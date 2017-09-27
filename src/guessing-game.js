fe476470c5190224a02c63a464ade034194cd6cd
Andrei Narkevich <NarkAndy@gmail.com>
2017-09-28 00:05:08 +0300

    guessing-game/feature: implement solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		this.num = null,
+        this.first = null,
+        this.last = null;
+    }
 
     setRange(min, max) {
-
+        if (min < max ) {
+			this.first = min;
+			this.last = max;
+		}
+		return this;
     }
 
     guess() {
-
+        this.num = Math.round((this.first + this.last) / 2); //or math floor
+        return this.num;
     }
 
     lower() {
-
+        this.last = this.num;
+        return this;
     }
 
     greater() {
-
+        this.first = this.num;
+        return this;
     }
 }
 
